a81be8d925353bdeabfa9352ef4467b6b1fd31ff
Michael Casey <mcasey8540@gmail.com>
2014-05-24 12:22:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cointrak::Application.config.secret_key_base = '5c254d5a5ca72c4e78cd426a4fd060819af74d168598428003e31aeb627460df23d898e85d4cfbfe3f9dc929ac7dc6a5343ba6c2e2c079b71485895fbf32a963'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Cointrak::Application.routes.draw do
+
+  root 'home#index'
+
+end
